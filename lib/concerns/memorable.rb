5a4d660ffc67594d18379f53659d63a702b4c094
Olivia Cubela <oliviacubela@gmail.com>
2020-07-16 17:29:48 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,8 +1,8 @@
 module Memorable
-  module ClassMethods
-    def reset_all
-      self.all.clear
-    end
+  module ClassMethods #Instead of building a brand new module to house this code from our '.initialize' methods, we need to think about behavior of code
+    def reset_all     #it's responsible for telling a class to keep track of its' own instances, which is similar behavior to 'reset_all'&'count'methods
+      self.all.clear   #already in 'Memorable' module. Nest the 'InstanceMethods'module in the Memorable module.
+    end               #we can 'include' or 'extend' individual modules as needed
 
     def count
       self.all.count
